5b8c04683f6f683c90e753589c35177774bb8496
Srinivasa Kalyan Chakravarthy Putchala <sputchala@paloaltonetworks.com.com>
2018-07-10 16:11:53 -0700

    initial

---
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -0,0 +1,11 @@
+package artifactory.test;
+
+/**
+ * Hello world!
+ */
+public class Multi3 {
+    public static void main(String[] args) {
+        new Multi3();
+        System.out.println("Hello World!");
+    }
+}
